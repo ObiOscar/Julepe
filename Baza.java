01c87cd9479ac35f0e2ac92f6b2ffc6c4289b7d4
Óscar Fernández <oscar.fer.rodz@gmail.com>
2017-02-02 14:11:06 +0100

    Mejoro el ultimo metodo

--- Baza.java
+++ Baza.java
@@ -6,6 +6,8 @@ public class Baza
     private ArrayList<Carta> cartasDeLaBaza;    // Creo un ArrayList de la clase Carta y lo llamo cartas del Mazo
     private ArrayList<String> nombreJugador;    // ArrayList con los nombres de los jugadores
     private Jugador[]jugadoresEnPartida;        // Jugadores que juegan    
+    
+    private String nombreGanador;                      //Guarda nombre del ganador
 
     /**
      * Tenga un constructor que admita dos parametros. El primero es un entero que indica el número de jugadores que van a tirar carta y el segundo es otro entero que indica el palo que pinta.
@@ -26,61 +28,49 @@ public class Baza
      * El segundo parámetro indica el nombre del jugador que tiro la carta. El método no devuelve nada.
      */
     public void addCarta(Carta cartaLanzadaJugador, String nombreJugadores){
-	   cartasDeLaBaza.add(cartaLanzadaJugador);  //No tiene mucho sentido, guarda la carta en el ArrayList, independientemente de que la carta la tenga el jugador
-	   nombreJugador.add(nombreJugadores);
+       cartasDeLaBaza.add(cartaLanzadaJugador);  //No tiene mucho sentido, guarda la carta en el ArrayList, independientemente de que la carta la tenga el jugador
+       nombreJugador.add(nombreJugadores);
        /*for(Jugador jugador : jugadoresEnPartida){
-			if(jugador.getNombre().equals(nombre)){
+            if(jugador.getNombre().equals(nombre)){
                cartasDeLaBaza.add(jugador.tirarCarta(cartaLanzadaJugador.toString()));
             }
-   	   }*/
+       }*/
     }
     
     /**Tenga un método llamado getPaloPrimeraCartaDeLaBaza sin parámetros que devuelva un entero que representa el palo 
      *de la primera carta añadida a la baza o -1 en caso de que no se haya añadido aún ninguna carta.*/
     public int getPaloPrimeraCartaDeLaBaza(){
-		int paloPrimeraCartaBaza = -1;
-		if (cartasDeLaBaza.size() > 0){
-			paloPrimeraCartaBaza = cartasDeLaBaza.get(0).getVpaloCarta();
-		}
-		return paloPrimeraCartaBaza;
-	}
+        int paloPrimeraCartaBaza = -1;
+        if (cartasDeLaBaza.size() > 0){
+            paloPrimeraCartaBaza = cartasDeLaBaza.get(0).getVpaloCarta();
+        }
+        return paloPrimeraCartaBaza;
+    }
 
-	/**Tenga un método llamado cartaQueVaGanandoLaBaza sin parámetros que devuelva un objeto Carta.
-	* Este objeto carta es la carta que va ganando la baza conforme a las normas del julepe. 
-	*En caso de que la baza este vacía este método devuelve null.
-	*/
-	public Carta cartaQueVaGanandoLaBaza(){
-		Carta comprobarCarta = null;
-		for(int contador = 0; contador < cartasDeLaBaza.size(); contador++){
-			if(comprobarCarta == null){
-				comprobarCarta = cartasDeLaBaza.get(0);
-			}			
-			if (!comprobarCarta.ganaA(cartasDeLaBaza.get(contador), paloPinta)){
-				comprobarCarta = cartasDeLaBaza.get(contador);
-			}
-		}	
-		return comprobarCarta;
-	}
+    /**Tenga un método llamado cartaQueVaGanandoLaBaza sin parámetros que devuelva un objeto Carta.
+    * Este objeto carta es la carta que va ganando la baza conforme a las normas del julepe. 
+    *En caso de que la baza este vacía este método devuelve null.
+    */
+    public Carta cartaQueVaGanandoLaBaza(){
+        Carta comprobarCarta = null;
+        for(int contador = 0; contador < cartasDeLaBaza.size(); contador++){
+            if(comprobarCarta == null){
+                comprobarCarta = cartasDeLaBaza.get(0);
+            }           
+            if (!comprobarCarta.ganaA(cartasDeLaBaza.get(contador), paloPinta)){
+                comprobarCarta = cartasDeLaBaza.get(contador);
+                nombreGanador = nombreJugador.get(contador);
+            }
+        }   
+        return comprobarCarta;
+    }
 
-	/**Tenga un método llamado nombreJugadorQueVaGanandoLaBaza que no admite parámetros y que devuelve el nombre del jugador 
-	 *que va ganando la baza en el momento actual.
-	*/
-	public String nombreJugadorQueVaGanandoLaBaza (){
-		String nombreGanador = "";
-		Carta comprobarCarta = null;
-
-		int contador = 0;
-		while(contador < cartasDeLaBaza.size()){
-			if(comprobarCarta == null){
-				comprobarCarta = cartasDeLaBaza.get(0);
-			}			
-			if (!comprobarCarta.ganaA(cartasDeLaBaza.get(contador), paloPinta)){
-				comprobarCarta = cartasDeLaBaza.get(contador);
-				nombreGanador = nombreJugador.get(contador);
-			}
-			contador++;
-		}	
-		return nombreGanador;
-	}
+    /**Tenga un método llamado nombreJugadorQueVaGanandoLaBaza que no admite parámetros y que devuelve el nombre del jugador 
+     *que va ganando la baza en el momento actual.
+    */
+    public String nombreJugadorQueVaGanandoLaBaza (){
+        cartaQueVaGanandoLaBaza();
+        return nombreGanador;
+    }
 
 }
